19f7e72c655c84c2f4f0abf0fd5b8ee06d018c41
Kinocas <717koushi717@gmail.com>
2021-08-26 10:13:13 +0900

    編集・削除機能の追加

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,25 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to root_path
+  end
+
   private
   def movie_params
     params.require(:movie).permit(:movie_title, :image, :thought, :genre_id, :evaluation).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "movies#index"
-  resources :movies, only: [:index, :new, :create, :show]
+  resources :movies
 end
