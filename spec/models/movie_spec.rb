a1d81559adbe4018b7dce57e1834094c14e26098
Kinocas <717koushi717@gmail.com>
2021-09-16 20:32:11 +0900

    単体テストの修正

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -27,47 +27,47 @@ RSpec.describe Movie, type: :model do
     it 'movie_titleが空では登録できない' do
       @movie.movie_title = ''
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Movie title can't be blank"
+      expect(@movie.errors.full_messages).to include "タイトルを入力してください"
     end
     it 'thoughtが空では登録できない' do
       @movie.thought = ''
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Thought can't be blank"
+      expect(@movie.errors.full_messages).to include "感想を入力してください"
     end
     it 'genre_idが空では登録できない' do
       @movie.genre_id = ''
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Genre can't be blank"
+      expect(@movie.errors.full_messages).to include "ジャンルを入力してください"
     end
     it 'evaluationが空では登録できない' do
       @movie.evaluation = ''
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Evaluation can't be blank"
+      expect(@movie.errors.full_messages).to include "評価を入力してください"
     end
     it 'releaseが空では登録できない' do
       @movie.release = ''
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Release can't be blank"
+      expect(@movie.errors.full_messages).to include "公開を入力してください"
     end
     it 'userが空では登録できない' do
       @movie.user = nil
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "User must exist"
+      expect(@movie.errors.full_messages).to include "ユーザーを入力してください"
     end
     it 'genre_idが数値以外では登録できない' do
       @movie.genre_id = 'a'
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Genre is not a number"
+      expect(@movie.errors.full_messages).to include "ジャンルは数値で入力してください"
     end
     it 'evaluationが0より小さいと登録できない' do
       @movie.evaluation = -1
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Evaluation must be greater than or equal to 0"
+      expect(@movie.errors.full_messages).to include "評価は0以上の値にしてください"
     end
     it 'evaluationが5より大きいと登録できない' do
       @movie.evaluation = 6
       @movie.valid?
-      expect(@movie.errors.full_messages).to include "Evaluation must be less than or equal to 5"
+      expect(@movie.errors.full_messages).to include "評価は5以下の値にしてください"
     end
   end
 end
